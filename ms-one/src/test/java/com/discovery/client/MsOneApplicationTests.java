bc245190a4295145cc7e51f0a5a4f9c5b421155a
Prateek <prateek.p@newstreettech.com>
2024-05-08 16:08:08 +0530

    Add Config Server

--- ms-one/src/test/java/com/discovery/client/MsOneApplicationTests.java
+++ ms-one/src/test/java/com/discovery/client/MsOneApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryClientApplicationTests {
+class MsOneApplicationTests {
 
 	@Test
 	void contextLoads() {
