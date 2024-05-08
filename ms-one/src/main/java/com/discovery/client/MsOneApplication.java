d4960241ea96a2f1c18ba1f5fd455c1a0617e950
Prateek <prateek.p@newstreettech.com>
2024-05-08 16:08:08 +0530

    Add Config Server

---
+++ ms-one/src/main/java/com/discovery/client/MsOneApplication.java
@@ -0,0 +1,28 @@
+package com.discovery.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class MsOneApplication {
+
+	@Autowired
+	Environment environment;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOneApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+			System.out.println("@@@@@@@@@@@");
+			System.out.println(environment.getProperty("build.version"));
+			System.out.println(environment.getProperty("accounts.message"));
+		};
+	}
+}
