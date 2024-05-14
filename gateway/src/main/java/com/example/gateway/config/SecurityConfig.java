d4eb49612e4b2efd77f96505b5ee1bbcf0b80889
Prateek <prateek.p@newstreettech.com>
2024-05-14 17:39:01 +0530

    add authentication and authorization

--- gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -2,10 +2,18 @@ package com.example.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,9 +24,22 @@ public class SecurityConfig {
         httpSecurity.authorizeExchange(authorizeExchangeSpec ->
             authorizeExchangeSpec
                     .pathMatchers("/ms-two/**").permitAll()
-                    .pathMatchers("/ms-one/**").hasRole("ADMIN"));
+                    .pathMatchers("/ms-one/**").hasRole("EDITOR"))
+
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
+
+//        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+
 
         httpSecurity.csrf(csrfSpec -> csrfSpec.disable());
         return httpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        System.out.println("######################");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
